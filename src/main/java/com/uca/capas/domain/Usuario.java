87bd2505cd348f23e08f72415a45d64fac9d6704
CarMer194 <00060313@uca.edu.sv>
2020-04-05 19:24:06 -0600

    first commit

---
+++ src/main/java/com/uca/capas/domain/Usuario.java
@@ -0,0 +1,28 @@
+package com.uca.capas.domain;
+
+public class Usuario {
+	private String nombre;
+	private String ocupacion;
+	private String prueba;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getOcupacion() {
+		return ocupacion;
+	}
+	public void setOcupacion(String ocupacion) {
+		this.ocupacion = ocupacion;
+	}
+	public String getPrueba() {
+		return prueba;
+	}
+	public void setPrueba(String prueba) {
+		this.prueba = prueba;
+	}
+	
+	
+}
